ecaeba5badaef4b3fa669caf2e7b6bb1183eb987
devansh <devanshkhamar@gmail.com>
2021-07-15 17:55:57 +0530

    change in sum.java

--- sum.java
+++ sum.java
@@ -1,7 +1,7 @@
 public class sum{
 	public static void main(String[] a){
-		int nume =1 , num=2, sum;
-		sum=nume+num;
-		System.out.println(sum);
+		int num1 =10 , num2=20, sum;
+		sum=num1+num2;
+		System.out.println("The sum is: "+ sum);
 		}
 }
